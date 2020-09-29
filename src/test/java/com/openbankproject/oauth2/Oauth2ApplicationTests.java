a8f50008c5d25512141361d51c1061c3c3698be1
shuang <shuang@tesobe.com>
2020-09-30 00:05:00 +0800

    omit not used test

--- src/test/java/com/openbankproject/oauth2/Oauth2ApplicationTests.java
+++ src/test/java/com/openbankproject/oauth2/Oauth2ApplicationTests.java
@@ -3,7 +3,7 @@ package com.openbankproject.oauth2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Oauth2ApplicationTests {
 
     @Test
